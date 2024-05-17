e0ccdc00e37ec4d6b953116ff9a44d13946c8497
Kazuya Umeki <kazuyaumeki1115@gmail.com>
2024-05-18 03:11:40 +0900

    定数ファイル.

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,9 +1,9 @@
-export interface FilteredItem {
+export interface FilterItem {
   key: string;
   name: string;
 }
 
-export const filteredItems: FilteredItem[] = [
+export const filterItems: FilterItem[] = [
   { key: 'inbox', name: 'Inbox' },
   { key: 'today', name: 'Today' },
   { key: 'week', name: 'Next 7 Days' },
