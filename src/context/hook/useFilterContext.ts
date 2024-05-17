c4013ba0bff08a3bda9a275790131c3adc875900
Kazuya Umeki <kazuyaumeki1115@gmail.com>
2024-05-18 03:21:40 +0900

    関数名変更.

--- src/context/hook/useFilterContext.ts
+++ src/context/hook/useFilterContext.ts
@@ -1,4 +1,4 @@
 import {useContext} from 'react';
 import FilterContext from '../FilterContext';
 
-export const useProjectContext = () => useContext(FilterContext);
+export const useFilterContext = () => useContext(FilterContext);
