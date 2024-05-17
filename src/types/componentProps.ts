fe477b5e8bd5824f34960da572d6048f2f343915
Kazuya Umeki <kazuyaumeki1115@gmail.com>
2024-05-17 19:53:22 +0900

    型定義ファイル作成。

---
+++ src/types/componentProps.ts
@@ -0,0 +1,9 @@
+import { Project, Task } from './models';
+
+export interface ProjectListProps {
+  projects: Project[];
+}
+
+export interface TaskListProps {
+  tasks: Task[];
+}
---
+++ src/types/models.ts
@@ -0,0 +1,22 @@
+export type Project = {
+  docId: string,
+  name: string,
+  projectId: string,
+  userId: string,
+};
+
+export type Task = {
+  docId: string,
+  name: string;
+  projectId: string;
+  date: string;
+  archived: boolean;
+};
+
+export type User = {
+  docId: string,
+  firstName: string;
+  lastName: string;
+  twitter: string;
+  userId: string;
+};
