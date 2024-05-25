8310ec0f1ed7ac595e82cda0417ed9e10cd34691
Kazuya Umeki <kazuyaumeki1115@gmail.com>
2024-05-26 03:17:30 +0900

    タスク取得ロジックの修正.

--- src/hook/useTasks.ts
+++ src/hook/useTasks.ts
@@ -7,18 +7,24 @@ const {VITE_USER_ID} = import.meta.env;
 
 export const useTasks = ({
   projectId,
-  isArchive = false,
+  isArchive,
 }: {
-  projectId: string|null,
+  projectId: string | null;
   isArchive: boolean;
 }) => {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   useEffect(() => {
+    if (projectId === null) {
+      setTasks([]);
+
+      return;
+    }
+
     const unsubscribe = firestore
       .collection('tasks')
       .where('userId', '==', VITE_USER_ID)
-      .where('projectId', '==', projectId ?? '')
+      .where('projectId', '==', projectId)
       .where('archived', '==', isArchive)
       .onSnapshot((snapshot) => {
         const newTasks = snapshot.docs.map((doc) => ({
