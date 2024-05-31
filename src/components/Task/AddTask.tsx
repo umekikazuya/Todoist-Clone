df46ad3cb1f5d5134ac2c419a61492e8b901314d
Kazuya Umeki <kazuyaumeki1115@gmail.com>
2024-05-31 23:59:41 +0900

    Fixed.

--- src/components/Task/AddTask.tsx
+++ src/components/Task/AddTask.tsx
@@ -21,12 +21,11 @@ const projectDataToOptions = (projects: Project[]) => {
 
 const getNow = () => {
   const d = new Date();
-  let month = '' + (d.getMonth() + 1);
-  let day = '' + d.getDate();
+  let month = `${d.getMonth() + 1}`;
+  let day = `${d.getDate()}`;
   const year = d.getFullYear();
-
-  if (month.length < 2) month = '0' + month;
-  if (day.length < 2) day = '0' + day;
+  if (month.length < 2) month = `0${month}`;
+  if (day.length < 2) day = `0${day}`;
 
   return [year, month, day].join('-');
 };
